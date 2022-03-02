5648daf3d9d6b465ac3fed02f877f4916ebb83d7
tixvage <mel10h3421@gmail.com>
2022-03-02 22:23:33 +0300

    lets goo

---
+++ GAP/src/main.cpp
@@ -0,0 +1,13 @@
+#include "raylib.h"
+
+int main(){
+    InitWindow(1080, 720, "GAP");
+    SetTargetFPS(120);
+
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawFPS(20, 20);
+        EndDrawing();
+    }
+}
